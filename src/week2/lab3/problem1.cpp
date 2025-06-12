a3c4275e865abf540f1209253ed2d73e72b84107
2110159 <safoyevshahboz@gmail.com>
2025-06-12 16:09:16 +0500

    push

--- src/week2/lab3/problem1.cpp
+++ src/week2/lab3/problem1.cpp
@@ -1,3 +1,25 @@
-//
-// Created by Shakhboz Safoev on 12/06/25.
-//
+#include <iostream>
+using namespace std;
+
+int main() {
+    char signal;
+
+    cout << "enter: g for green, y for yellow, r for red: ";
+    cin >> signal;
+
+    switch (signal) {
+        case 'g':
+            cout << "go!" << endl;
+            break;
+        case 'y':
+            cout << "get ready!" << endl;
+            break;
+        case 'r':
+            cout << "stop" << endl;
+            break;
+        default:
+            cout << "wrong input. please enter g, y, or r." << endl;
+    }
+
+    return 0;
+}
