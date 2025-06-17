20cfe4c680cd7b5b3577c1c169906c42d3189703
2110159 <safoyevshahboz@gmail.com>
2025-06-17 16:14:19 +0500

    lab7 problems

---
+++ src/week3/lab7/problem6.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+void reverseDigits(int &x) {
+    int reversed = 0;
+    int original = x;
+
+    bool isNegative = (x < 0);
+    if (isNegative) x = -x;
+
+    while (x > 0) {
+        reversed = reversed * 10 + x % 10;
+        x /= 10;
+    }
+
+    x = isNegative ? -reversed : reversed;
+}
+
+int main() {
+    int num;
+    cout << "number: ";
+    cin >> num;
+
+    reverseDigits(num);
+
+    cout << "reversed number: " << num << endl;
+
+    return 0;
+}
