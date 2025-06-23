e591fde9528990390cf97b74faecc03439c1761c
2110159 <safoyevshahboz@gmail.com>
2025-06-23 15:38:19 +0500

    lab10 problems

---
+++ src/week4/lab10/problem1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "n numbers: ";
+    cin >> n;
+    vector<int> numbers(n);
+    int sum = 0;
+
+    cout << "enter " << n << " numbers: " << endl;
+    for (int i = 0; i < n; ++i) {
+        cin >> numbers[i];
+        sum += numbers[i];
+    }
+
+    cout << "sum = " << sum << endl;
+
+    return 0;
+}
